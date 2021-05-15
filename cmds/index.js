9e9cfe2fbc2cf4ecdd98c144b9f4320dcaa4a95a
Manik Jain <jain.manik@yahoo.co.in>
2021-05-15 18:07:36 -0400

    removing unused/test code

--- cmds/index.js
+++ cmds/index.js
@@ -2,8 +2,4 @@ import CardanoCliExecutor from './CardanoCliExecutor.js'
 import Commands from './commands.js'
 
 let cardanoCli = new CardanoCliExecutor();
-//cardanoCli.execute(Commands.Commands.LIST_FILES);
-
-let t = `curl -H 'project_id: yIiASEfGiDNeVjPhydXIjRX8Y5Mdw2PA' https://cardano-testnet.blockfrost.io/api/v0/txs/e13b1ec55e64d0807895ba81435e4275839dd57b5bb915d924b58eb3e6b1f390/metadata`;
-cardanoCli.execute(t);
-console.log(Commands)
+cardanoCli.execute(Commands.Commands.LIST_FILES);
